4fbe1ada875f3fbce58267b02eb0b5ca2839a88d
Maxim Mineev <mms1401@ya.ru>
2024-11-11 20:23:44 +0500

    Использование распределения в CircularCloudLayouter, проверка на НЕ пересечение прямоугольников

--- cs/TagsCloudTests/CircularCloudLayouterTests.cs
+++ cs/TagsCloudTests/CircularCloudLayouterTests.cs
@@ -13,8 +13,27 @@ public class CircularCloudLayouterTests
     {
         var rectSize = new SizeF(100, 100);
         var center = new PointF(10, 10);
-        var layouter = new CircularCloudLayouter(center);
+        var layouter = new CircularCloudLayouter(center, new SpiralPointDistributor(center));
         var rect = layouter.PutNextRectangle(rectSize);
         rect.GetCenter().Should().Be(center);
+    }
+
+    [Test]
+    public void PutNextRectangle_Rectangles_ShouldNotIntersect()
+    {
+        var center = new PointF(0, 0);
+        var rnd = new Random();
+        var layouter = new CircularCloudLayouter(center, new SpiralPointDistributor(center));
+        var rectangles = new List<RectangleF>();
+        for (var i = 0; i < 10; i++)
+        {
+            var rect = layouter.PutNextRectangle(new SizeF(rnd.Next(1, 20), rnd.Next(1, 20)));
+            rectangles.Add(rect);
+        }
+
+        for (var i = 0; i < rectangles.Count; i++)
+        for (var j = i; j < rectangles.Count; j++)
+            if (i != j)
+                rectangles[i].IntersectsWith(rectangles[j]).Should().BeFalse($"{rectangles[i]}, {rectangles[j]}");
     }
 }
--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++ cs/TagsCloudVisualization/CircularCloudLayouter.cs
@@ -4,14 +4,24 @@ namespace TagsCloudVisualization;
 
 public class CircularCloudLayouter
 {
-    private readonly PointF center;
+    private readonly IPointDistributor distributor;
+    private readonly List<RectangleF> rectangles = [];
 
-    public CircularCloudLayouter(PointF center)
-        => this.center = center;
+    public CircularCloudLayouter(PointF center, IPointDistributor distributor)
+        => this.distributor = distributor;
 
     public RectangleF PutNextRectangle(SizeF rectangleSize)
     {
-        var rect = RectangleFExtensions.GetRectangleFWithCenterIn(center, rectangleSize);
+        PointF point;
+        RectangleF rect;
+        do
+        {
+            point = distributor.GetNextPoint();
+            rect = RectangleFExtensions.GetRectangleFWithCenterIn(point, rectangleSize);
+        } while (rectangles.Any(r => r.IntersectsWith(rect)));
+
+        rectangles.Add(rect);
+        Console.WriteLine(rect);
         return rect;
     }
 }
